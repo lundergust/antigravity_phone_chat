e66738f9eb30f7c79ab995926bc90e03830a551d
lundergust <44384018+lundergust@users.noreply.github.com>
2026-02-15 12:41:38 -0600

    Create RepoView.tsx

---
+++ web/src/features/repo/RepoView.tsx
@@ -0,0 +1,25 @@
+import RepoTree from "./RepoTree";
+import FileViewer from "./FileViewer";
+
+export default function RepoView() {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "160px 1fr",
+        height: "100%",
+      }}
+    >
+      <div
+        style={{
+          borderRight: "1px solid var(--color-border)",
+          overflowY: "auto",
+        }}
+      >
+        <RepoTree />
+      </div>
+
+      <FileViewer />
+    </div>
+  );
+}
