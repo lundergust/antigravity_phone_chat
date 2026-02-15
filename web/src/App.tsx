f28660e937b2460bec703653d8d862eff0f444d4
lundergust <44384018+lundergust@users.noreply.github.com>
2026-02-15 12:42:53 -0600

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import AppShell from "./shell/AppShell";
 import ChatView from "./features/chat/ChatView";
+import RepoView from "./features/repo/RepoView";
 
 export default function App() {
   return (
     <AppShell>
       <ChatView />
+      <RepoView />
     </AppShell>
   );
 }
