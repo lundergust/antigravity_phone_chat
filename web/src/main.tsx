be552df84d4ed7da7f4237cb435d07363c0a8a6e
lundergust <44384018+lundergust@users.noreply.github.com>
2026-02-15 14:28:10 -0600

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/reset.css";
-import "./design/tokens.css";
+import "./index.css";
 
-ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
