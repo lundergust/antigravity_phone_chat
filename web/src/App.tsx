42f58b06773ed7c5f584f0f2773b066beef42757
lundergust <44384018+lundergust@users.noreply.github.com>
2026-02-15 12:19:53 -0600

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,10 @@
 import AppShell from "./shell/AppShell";
+import ChatView from "./features/chat/ChatView";
 
 export default function App() {
-  return <AppShell> {/* Insert feature content here later */} </AppShell>;
+  return (
+    <AppShell>
+      <ChatView />
+    </AppShell>
+  );
 }
